9c96cf7bdc46f20503fce1397ff69e0ad46fa8b7
neobassist <77500745+neobassist@users.noreply.github.com>
2025-07-25 10:56:18 +0900

    Add files via upload

---
+++ label_map.py
@@ -0,0 +1,15 @@
+# label_map.py
+# Class ID to label name mapping used in Cambrian carbonate microfacies dataset
+
+label_map = {
+    0: "intraclast",
+    1: "thromboid",
+    2: "peloid",
+    3: "micrite",
+    4: "spicule",
+    5: "calcisphere",
+    6: "bioclast",
+    7: "cement",
+    8: "matrix",
+    9: "void",
+}
